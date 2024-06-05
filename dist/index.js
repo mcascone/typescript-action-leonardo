a2621ca62735fe14cfa01f41bd0daa6cfd0e15ff
mcascone <max@bitovi.com>
2024-06-05 15:20:57 -0500

    add ncc for dependencies3

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,5 @@
 "use strict";
-console.log('Hello World!');
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const name = (0, core_1.getInput)('name');
+console.log(`Hello ${name}!`);
